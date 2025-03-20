1e0f9a0eba3a21d74360bd92733b5195c02b7f84
floppa <jbarek2027@gmail.com>
2025-03-19 19:22:40 -0700

    Done

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ Everyone counts.
 Everyone has access to great learning.
 Every person's unique talents are valued equally.
 Everyone takes responsibility for themselves and their community."""
+ 
+char_count = {}
 
-# Make a dictionary where the keys are characters and the values are the number of times that character appears in the `text` print the dicitonary
+for char in text:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1
+
+print(char_count)
